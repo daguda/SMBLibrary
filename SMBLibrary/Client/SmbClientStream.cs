7e5c0390ca278d85d322e056654f821757ce14cb
Hagaygo <hagay.goshen@gmail.com>
2018-04-08 11:16:49 +0300

    first working file reader

--- SMBLibrary/Client/SmbClientStream.cs
+++ SMBLibrary/Client/SmbClientStream.cs
@@ -8,18 +8,21 @@ namespace SMBLibrary.Client
     public class SMBClientStream : System.IO.Stream
     {
         public override bool CanRead { get { return true; } }
-        public override bool CanSeek { get { return false; } }
+        public override bool CanSeek { get { return true; } }
         public override bool CanWrite { get; }
-        public override long Length { get; }
+        public override long Length { get { return _length; } }
         public override long Position { get; set; }
 
         ISMBFileStore _fileStore;
         object _handle;
+        long _length;
 
         public SMBClientStream(ISMBFileStore fileStore, object handle)
         {
             _fileStore = fileStore;
             _handle = handle;
+            _fileStore.GetFileInformation(out var result, _handle, FileInformationClass.FileStandardInformation);
+            _length = ((FileStandardInformation)result).EndOfFile;
         }
 
         public override void Flush()
@@ -30,16 +33,36 @@ namespace SMBLibrary.Client
         public override int Read(byte[] buffer, int offset, int count)
         {
             var myBuffer = new byte[count];
-            var status = _fileStore.ReadFile(out myBuffer, _handle, offset, count);
-            myBuffer.CopyTo(buffer,0);
-            if (status == NTStatus.STATUS_SUCCESS)
-                return count;
+            if (count + offset + Position > Length)
+                count = (int)(Length - (Position));
+            var status = _fileStore.ReadFile(out myBuffer, _handle, Position, count);
+            if (myBuffer != null)
+            {
+                myBuffer.CopyTo(buffer, 0);
+                if (status == NTStatus.STATUS_SUCCESS)
+                {
+                    Position += count;
+                    return count;
+                }
+            }
             throw new SMBClientException("Error on read") { NTStatus = status };
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            throw new NotImplementedException();
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    Position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    Position += offset;
+                    break;
+                case SeekOrigin.End:
+                    Position = Length - offset;
+                    break;
+            }
+            return Position;
         }
 
         public override void SetLength(long value)
